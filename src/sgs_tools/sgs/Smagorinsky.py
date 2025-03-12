99a47aeaad6853b0ad7bcdcb8e2a7be11acbfe1e
Dimitar Vlaykov <d.vlaykov@exeter.ac.uk>
2025-03-12 11:25:09 +0000

    assertion message

--- src/sgs_tools/sgs/Smagorinsky.py
+++ src/sgs_tools/sgs/Smagorinsky.py
@@ -11,7 +11,7 @@ from .sgs_model import DynamicHeatModel, DynamicVelocityModel, SGSModel
 # check that arr is uniform along `filter_dims` with spacing of `dx`
 def _assert_coord_dx(filter_dims: list[Hashable], arr: xr.DataArray, dx: float) -> None:
     for c in filter_dims:
-        assert (arr[c].diff(dim=c) == dx).all()
+        assert (arr[c].diff(dim=c) == dx).all(), f"Not uniform dimension {c}: {arr[c]}"
 
 
 @dataclass(frozen=True)

b752cf498e639202cfb13e60dc62f17671046219
Dimitar Vlaykov <d.vlaykov@exeter.ac.uk>
2025-07-01 14:32:44 +0100

    new: basic NetCDF Writer object, exposes overwrite bhviour

---
+++ src/sgs_tools/io/netcdf_writer.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import xarray as xr
+from ..util.timer import timer
+
+@dataclass
+class NetCDFWriter:
+    overwrite: bool = False
+
+    def check_filename(self, filename: Path) -> bool:
+        return filename.exists()
+
+    def write(self, array: xr.Dataset, filename: Path) -> None:
+        if self.check_filename(filename):
+            if not self.overwrite:
+                raise OSError(f"{filename} already exists. Won't overwrite.")
+        filename.parent.mkdir(exist_ok=True, parents=True)
+        array.to_netcdf(
+            filename,
+            mode="w",
+            compute=True,
+            engine="h5netcdf",
+        )
+

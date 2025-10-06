3de6b9075c145e9718e44d387ed2f2af01b87837
Dimitar Vlaykov <d.vlaykov@exeter.ac.uk>
2025-10-06 17:43:02 +0100

    fix: gitinfo for untracked files

--- src/sgs_tools/util/gitinfo.py
+++ src/sgs_tools/util/gitinfo.py
@@ -26,30 +26,18 @@ def _root(start=None):
 
 
 def _diff_tracked(root):
-    diff = _git(["diff"], cwd=root)
-    if not diff:
-        return ""
-    lines, skip = [], False
-    for line in diff.splitlines():
-        if line.startswith("diff --git") and line.endswith(".ipynb"):
-            skip = True
-            continue
-        if skip and line.startswith("diff --git"):
-            skip = False
-        if not skip:
-            lines.append(line)
-    return "\n".join(lines).strip()
+    diff = _git(["diff", "HEAD", "--", ":(exclude)*.ipynb"], cwd=root)
+    return diff.strip()
 
 
 def _diff_untracked(root):
     files = _git(["ls-files", "--others", "--exclude-standard"], cwd=root)
-    # skip .ipynb files -- too large diffs
-    files = [f for f in files.splitlines() if not f.endswith(".ipynb")]
-    diffs = ""
-    if files:
-        diff = _git(["diff", "--no-index", "/dev/null", *files], cwd=root)
-        if diff:
-            diffs.append(diff)
+    files = [f for f in files.splitlines() if f and not f.endswith(".ipynb")]
+    diffs = []
+    for f in files:
+        git_diff = _git(["diff", "--no-index", "/dev/null", f], cwd=root)
+        if git_diff:
+            diffs.append(git_diff)
     return "\n".join(diffs).strip()
 
 
@@ -75,17 +63,13 @@ def get_git_state(verbosity=0):
 
     # 3 Full debug/state logging
     if verbosity >= 2:
-        diff = ""
         tracked = _diff_tracked(root)
         if tracked:
-            diff += tracked
+            out["Tracked"] = tracked
 
         untracked = _diff_untracked(root)
         if untracked:
-            diff += "\nUntracked:\n"
-            diff += untracked
-        # diff = 'n'.join(diff).strip()
-        out["Changes"] = diff.strip()
+            out["Untracked"] = untracked
     return out
 
 
@@ -97,21 +81,21 @@ def print_git_state(verbosity=0):
 def write_git_diff_file(git_info: dict[str, str], out_path: Path | str = ".") -> str:
     """
     Write full Git info (Files + Changes) to an external JSON file.
-    Relies on existence of Changes and Commit keys.
+    Relies on existence of Files and Commit keys.
     :param git_info: Dictionary returned by get_git_state().
     :param out_path: Location of output JSON file. Will create if missing.
     :return: Path to the written JSON file.
     """
     state = {k: v.strip().splitlines() for k, v in git_info.items()}
-    state_is_dirty = bool(state.get("Changes", ""))
+    state_is_dirty = bool(state.get("Files", ""))
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    state["date-time"] = [timestamp]
 
     fname = f"sgs_tools_v{state['Commit'][0]}.json"
     full_path = Path(out_path) / fname
     full_path.parent.mkdir(parents=True, exist_ok=True)
     # add a run-time time-stamp for different dirty versions
     if full_path.exists() and state_is_dirty:
-        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-        state["date-time"] = [timestamp]
         full_path = full_path.with_stem(f"{full_path.stem}_{timestamp}")
 
     with open(full_path, "w") as f:
